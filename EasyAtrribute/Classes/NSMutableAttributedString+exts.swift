9a1b0a3a9bce57f67e86c91b51611c3705bf10cc
杜哲 <363958265@qq.com>
2017-10-31 09:48:24 +0800

    修改属性名字

--- EasyAtrribute/Classes/NSMutableAttributedString+exts.swift
+++ EasyAtrribute/Classes/NSMutableAttributedString+exts.swift
@@ -14,7 +14,7 @@ public func + (attr1 : NSMutableAttributedString ,attr2 : NSMutableAttributedStr
 }
 
 public extension NSMutableAttributedString{
-  public func allRange()->NSRange{
+  public func ez_allRange()->NSRange{
     return NSMakeRange(0,self.length)
   }
   /// 添加图片
@@ -39,65 +39,65 @@ public extension NSMutableAttributedString{
   
   // 中划线
   public func strike(_ value: Int)->NSMutableAttributedString{
-    self.addAttributes([.strikethroughStyle:value], range: self.allRange())
+    self.addAttributes([.strikethroughStyle:value], range: self.ez_allRange())
     return self
   }
   // 中划线颜色
   public func strikeColor(_ color: UIColor)->NSMutableAttributedString{
-    self.addAttributes([.strikethroughColor:color], range: self.allRange())
+    self.addAttributes([.strikethroughColor:color], range: self.ez_allRange())
     return self
   }
   // 描边宽度
   public func strokeWidth(_ width: CGFloat)->NSMutableAttributedString{
-    self.addAttributes([.strokeWidth:width], range: self.allRange())
+    self.addAttributes([.strokeWidth:width], range: self.ez_allRange())
     return self
   }
   // 描边颜色
   public func strokeColor(_ color: UIColor)->NSMutableAttributedString{
-    self.addAttributes([.strokeColor:color], range: self.allRange())
+    self.addAttributes([.strokeColor:color], range: self.ez_allRange())
     return self
   }
   // 字间距
   public func fontSpace(_ space: CGFloat)->NSMutableAttributedString{
-    self.addAttributes([.kern:space], range: self.allRange())
+    self.addAttributes([.kern:space], range: self.ez_allRange())
     return self
   }
   // 背景色
   public func backgroundColor(_ color: UIColor)->NSMutableAttributedString{
-    self.addAttributes([.backgroundColor:color], range: self.allRange())
+    self.addAttributes([.backgroundColor:color], range: self.ez_allRange())
     return self
   }
   // 前景色
-  public func foreColor(_ color: UIColor)->NSMutableAttributedString{
-    self.addAttributes([.foregroundColor:color], range: self.allRange())
+  public func color(_ color: UIColor)->NSMutableAttributedString{
+    self.addAttributes([.foregroundColor:color], range: self.ez_allRange())
     return self
   }
   // 下划线
-  func underLine(_ style: NSUnderlineStyle)->NSMutableAttributedString{
-    self.addAttributes([.underlineStyle:style.rawValue], range: self.allRange())
+  func ez_underLine(_ style: NSUnderlineStyle)->NSMutableAttributedString{
+    self.addAttributes([.underlineStyle:style.rawValue], range: self.ez_allRange())
     return self
   }
   // 下划线颜色
-  func underLineColor(_ color: UIColor)->NSMutableAttributedString{
-    self.addAttributes([.underlineColor:color], range: self.allRange())
+  func ez_underLineColor(_ color: UIColor)->NSMutableAttributedString{
+    self.addAttributes([.underlineColor:color], range: self.ez_allRange())
     return self
   }
   // 字体
-  public func font(_ font: UIFont)->NSMutableAttributedString{
-    self.addAttributes([.font:font], range: self.allRange())
+  public func ez_font(_ font: UIFont)->NSMutableAttributedString{
+    self.addAttributes([.font:font], range: self.ez_allRange())
     return self
   }
   // 系统字体大小
   public func fontSize(_ size: CGFloat)->NSMutableAttributedString{
-    self.addAttributes([.font:UIFont.systemFont(ofSize: size)], range: self.allRange())
+    self.addAttributes([.font:UIFont.systemFont(ofSize: size)], range: self.ez_allRange())
     return self
   }
   // 行间距
-  public func lineSpace( _ space: CGFloat )->NSMutableAttributedString{
+  public func ez_lineSpace( _ space: CGFloat )->NSMutableAttributedString{
     let style = NSMutableParagraphStyle()
     style.lineSpacing = space
     style.lineBreakMode = .byCharWrapping
-    self.addAttribute(.paragraphStyle, value: style, range: self.allRange())
+    self.addAttribute(.paragraphStyle, value: style, range: self.ez_allRange())
     return self
   }
   public func toEz()->EZAttributeString{
--- Example/EasyAtrribute/ViewController.swift
+++ Example/EasyAtrribute/ViewController.swift
@@ -24,19 +24,19 @@ class ViewController: UIViewController {
       .removeAllAttribute()
       .appendAttributedText("温馨提示，谨防诈骗，跟您对话的可能是个骗子！！！ "
         .attribute()
-        .foreColor(UIColor.gray)
+        .color(UIColor.gray)
         .fontSize(15)
         .toEz())
       .appendAttributedText(" 拉黑 ".attribute()
-        .foreColor(UIColor.blue)
-        .font(UIFont.boldSystemFont(ofSize: 17))
+        .color(UIColor.blue)
+        .ez_font(UIFont.boldSystemFont(ofSize: 17))
         .toEz()
         .addAction{
           print("------这里是拉黑系统---------")
       })
       .appendAttributedText(" 举报 ".attribute()
-        .foreColor(UIColor.blue)
-        .font(UIFont.boldSystemFont(ofSize: 17))
+        .color(UIColor.blue)
+        .ez_font(UIFont.boldSystemFont(ofSize: 17))
         .toEz()
         .addAction{
           print("------这里是举报系统---------")
@@ -44,49 +44,49 @@ class ViewController: UIViewController {
     
     
     
-    label1.attributedText = "属性字"
-      .attribute()
-      .fontSize(18)
-      .foreColor(UIColor.blue)
-      .underLine(.styleSingle)
-      .underLineColor(UIColor.black)
-      +
-      (
-      " 测试一下"
-        .attribute()
-        .font(UIFont.boldSystemFont(ofSize: 20))
-        .foreColor(UIColor.red)
-        .underLine(.styleDouble)
-        .underLineColor(UIColor.blue)
-      +
-      " ￥".attribute()
-        .font(UIFont.boldSystemFont(ofSize: 23))
-        .foreColor(UIColor.black)
-      ).lineSpace(5)
-      +
-      " 2000"
-        .attribute()
-        .fontSize(19)
-        .foreColor(UIColor.purple)
-        .backgroundColor(UIColor.lightGray)
-        .fontSpace(3.5)
-      +
-      " 描边文字"
-        .attribute()
-        .fontSize(19)
-        .strokeColor(UIColor.blue)
-        .strokeWidth(3)
-      +
-      " 中划线 "
-        .attribute()
-        .fontSize(17)
-        .strike(2)
-        .strikeColor(UIColor.darkGray)
-      +
-      "带图片提示的文本"
-        .attribute()
-        .fontSize(15).foreColor(UIColor.lightGray)
-        .picture( #imageLiteral(resourceName: "warning") , bounds: CGRect(x: 0, y: 0, width: 13, height: 13), index: 0)
+//    label1.attributedText = "属性字"
+//      .attribute()
+//      .fontSize(18)
+//      .color(UIColor.blue)
+//      .ez_underLine(.styleSingle)
+//      .ez_underLineColor(UIColor.black)
+//      +
+//      (
+//      " 测试一下"
+//        .attribute()
+//        .ez_font(UIFont.boldSystemFont(ofSize: 20))
+//        .color(UIColor.red)
+//        .ez_underLine(.styleDouble)
+//        .ez_underLineColor(UIColor.blue)
+//      +
+//      " ￥".attribute()
+//        .font(UIFont.boldSystemFont(ofSize: 23))
+//        .color(UIColor.black)
+//      ).lineSpace(5)
+//      +
+//      " 2000"
+//        .attribute()
+//        .fontSize(19)
+//        .color(UIColor.purple)
+//        .backgroundColor(UIColor.lightGray)
+//        .fontSpace(3.5)
+//      +
+//      " 描边文字"
+//        .attribute()
+//        .fontSize(19)
+//        .strokeColor(UIColor.blue)
+//        .strokeWidth(3)
+//      +
+//      " 中划线 "
+//        .attribute()
+//        .fontSize(17)
+//        .strike(2)
+//        .strikeColor(UIColor.darkGray)
+//      +
+//      "带图片提示的文本"
+//        .attribute()
+//        .fontSize(15).foreColor(UIColor.lightGray)
+//        .picture( #imageLiteral(resourceName: "warning") , bounds: CGRect(x: 0, y: 0, width: 13, height: 13), index: 0)
   }
   
 }
